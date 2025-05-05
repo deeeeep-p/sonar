6f2dad4529b0877bd9e4fc78ab97c3e2d2ab8338
Deep <deeppatel223204@gmail.com>
2025-05-05 22:40:05 +0530

    test

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from app import add  # Import the add function from app.py
+
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-1, -2) == -3
+
+def test_add_mixed_numbers():
+    assert add(5, -2) == 3
+
+def test_add_zero():
+    assert add(10, 0) == 10
